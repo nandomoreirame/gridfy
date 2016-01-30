be8e36700abe5cfe3913720a77b51182f839ad4d
Fernando Moreira <nandomoreira.me@gmail.com>
2016-01-30 02:21:58 -0200

    compact css

--- config.rb
+++ config.rb
@@ -8,5 +8,5 @@ sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
-output_style = :expanded # or :nested or :compact or :compressed
+output_style = :compact # :expanded or :nested or :compact or :compressed
 line_comments = false
